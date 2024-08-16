0a2eb638ee841c0235bf29fca5406ff89719675b
ifeomagoody <goodnessezeokoli3@gmail.com>
2024-08-16 21:10:14 +0100

    I deleted some productController errors
    Signed-off-by: ifeomagoody <goodnessezeokoli3@gmail.com>

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -4,12 +4,7 @@ import { Request, Response } from 'express';
 import ProductModel from '../models/Product';
 
 export const getAllProducts = async (req: Request, res: Response) => {
-  try {
-    const products = await ProductModel.find();
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+  
 };
 
 
